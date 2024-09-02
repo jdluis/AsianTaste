20bd50ea72613d8fb2fa7930fd46f704656819b3
jdluis <jdluisdev@gmail.com>
2024-09-02 22:50:00 +0200

    FIX DEPLOY on Vercel

--- src/pages/ReservationPanel/index.tsx
+++ src/pages/ReservationPanel/index.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ReservationPanelProps {
     
 }
  
 const ReservationPanel: FC<ReservationPanelProps> = () => {
-    return (  );
+    return ( <>
+    </> );
 }
  
 export default ReservationPanel;
